5d2d4fd8a971eb02f0eec5f3a2a2e53ae5488149
Avani Anand <iamavanianand@gmail.com>
2024-05-19 13:45:57 +0530

    "learnt all types of insertion in doubly linked list"

--- DSA (code with harry)/doubly_list_insertion.c
+++ DSA (code with harry)/doubly_list_insertion.c
@@ -47,6 +47,43 @@ struct node *insertAtLast(struct node *head , int data){
 }
 
 
+struct node *insertAtIndex(struct node *head , int index ,int data){
+
+  struct node *ptr=(struct node *)malloc(sizeof(struct node));
+  struct node *p=head;
+  
+  int i=0;
+  while (i!= index-1)
+  {
+    p=p->next;
+    i++;
+  }
+  ptr->data=data;
+  ptr->next=p->next;
+  p->next=ptr;
+  ptr->prev=p;
+  return head;
+}
+
+
+
+struct node *insertAfterNode(struct node *head , struct node *prevNode ,int data){
+
+  struct node *ptr=(struct node *)malloc(sizeof(struct node));
+ 
+  
+  ptr->data=data;
+  
+  ptr->next=prevNode->next;
+  prevNode->next=ptr;
+  ptr->prev=prevNode;
+  return head;
+}
+
+
+
+
+
 int main(){
 struct node *n1=(struct node *)malloc(sizeof(struct node));
 struct node *n2=(struct node *)malloc(sizeof(struct node));
@@ -65,13 +102,15 @@ n3->next=n4;
 n3->prev=n2;
 n3->data=444;
 
-n4->next=NULL;
+n4->next=NULL; 
 n4->prev=n3;
 n4->data=4444;
 
  circularListForwardTraversal(n1);
 //  n1=insertAtFirst(n1,3333);
- n1=insertAtLast(n1,555555);
+//  n1=insertAtLast(n1,555555);
+//  n1=insertAtIndex(n1,3,10234);
+n1=insertAfterNode(n1,n3,234);
  circularListForwardTraversal(n1);
 
 
